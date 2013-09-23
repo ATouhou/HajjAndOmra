f1559899bda712f8899e7d44e8996d3310c9e013
muskteer <muskteer1983@gmail.com>
2013-09-23 13:15:00 +0200

    Item Details screen
    adding UI binding

--- app/controllers/ItemDetails.js
+++ app/controllers/ItemDetails.js
@@ -1,9 +1,9 @@
 var args = arguments[0] || {};
 
 if (args.get) {
-	$.header.text = args.get('name');
-	$.image.image = args.get('image');
-	$.details.text = args.get('details');
+	//$.header.text = args.get('name');
+	//$.image.image = args.get('image');
+	//$.details.text = args.get('details');
 
 } 
 
