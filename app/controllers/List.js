4791f68437b273f321a9cfae039c9435ebec1556
islamfotouh <eslam.a.elftoh@gmail.com>
2013-09-29 15:18:47 +0200

    category view item view

--- app/controllers/List.js
+++ app/controllers/List.js
@@ -1,6 +1,13 @@
 $.header.rightNavButton.addEventListener('click', function(){
 	Alloy.Globals.menu.toggleRightDrawer();
 });
-
-
-Alloy.Collections.Item.fetch();
+var args = arguments[0] || {};
+var catagoryID = args.type;
+var collection = Alloy.Collections.Item;
+collection.fetch({
+		// query: 'SELECT * FROM model WHERE columns
+		query: {
+			statement: 'SELECT * FROM Item WHERE catagoryID = ?',
+			params: [catagoryID]
+		}
+	});
