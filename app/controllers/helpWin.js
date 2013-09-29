9e1cf6e0c899e7af2b687afa29fa6359ff2ad3cc
islamfotouh <eslam.a.elftoh@gmail.com>
2013-09-29 15:17:48 +0200

    splash screen

---
+++ app/controllers/helpWin.js
@@ -0,0 +1,3 @@
+$.helpWin.addEventListener('click',function (argument) {
+  $.helpWin.close();
+});
--- app/controllers/home/home.js
+++ app/controllers/home/home.js
@@ -1,18 +1,29 @@
-
-$.titleBar.rightNavButton.addEventListener('click', function(){
+$.titleBar.rightNavButton.addEventListener('click', function() {
 	Alloy.Globals.menu.toggleRightDrawer();
 });
 
-
-
-Alloy.Globals.menu.on('open:[right]', function(){
+Alloy.Globals.menu.on('open:[right]', function() {
 	$.overlay.visible = true;
 });
 
-Alloy.Globals.menu.on('close:[right]', function(){
+Alloy.Globals.menu.on('close:[right]', function() {
 	$.overlay.visible = false;
 });
 
 function toggleMenu() {
 	Alloy.Globals.menu.toggleRightDrawer();
 }
+
+function splashScreen(argument) {
+	Ti.App.Properties.setBool('myBool', true);
+	var ctrl = Alloy.createController('helpWin');
+	ctrl.getView().open();
+}
+
+var result = Ti.App.Properties.getBool('myBool');
+if ( result == null ) {
+	splashScreen();
+} else {
+
+}
+
