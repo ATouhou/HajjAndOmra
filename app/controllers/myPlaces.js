4ccd4774231746c35f56c1b878aa53998c757c38
islamfotouh <eslam.a.elftoh@gmail.com>
2013-09-29 11:54:35 +0200

    HIJJOM-21 HIJJOM-22

--- app/controllers/myPlaces.js
+++ app/controllers/myPlaces.js
@@ -1,25 +1,16 @@
-var data=Alloy.Collections.MyPlaces;
+var data = Alloy.Collections.MyPlaces;
 data.fetch();
 
-var dummy;
-for(var i=0;i<10;i++){
-	dummy=Alloy.createModel('MyPlaces',{
-		name:'portsaid',
-		description:'my city',
-		longitude:32.283333,
-		latitude:31.250000
+function maping(e) {
+	var index = e.itemIndex;
+	var value = data.at(index);
+	var map = Alloy.createController('markedMap', {
+		details : value
 	});
-	data.add(dummy);
-}
-
-function maping(e){
-	var index=e.itemIndex;
-	var value=data.at(index);
-	var map=Alloy.createController('markedMap',{details:value});
 	map.getView().open();
 }
 
-$.header.rightNavButton.addEventListener('click', function(){
+$.header.rightNavButton.addEventListener('click', function() {
 	Alloy.Globals.menu.toggleRightDrawer();
 });
 
@@ -27,8 +18,7 @@ Ti.App.addEventListener('app:update', function(argument) {
 	data.fetch();
 });
 
-
 function openAddNew() {
-  var ctrl = Alloy.createController('AddNewPlaces');
-  ctrl.getView().open();
+	var ctrl = Alloy.createController('AddNewPlaces');
+	ctrl.getView().open();
 }
--- app/controllers/places.js
+++ app/controllers/places.js
@@ -1,24 +1,15 @@
-var data=Alloy.Collections.places;
+var data = Alloy.Collections.places;
 data.fetch();
 
-var dummy;
-for(var i=0;i<10;i++){
-	dummy=Alloy.createModel('MyPlaces',{
-		name:'portsaid',
-		description:'my city',
-		longitude:32.283333,
-		latitude:31.250000
+function maping(e) {
+	var index = e.itemIndex;
+	var value = data.at(index);
+	var map = Alloy.createController('markedMap', {
+		details : value
 	});
-	data.add(dummy);
-}
-
-function maping(e){
-	var index=e.itemIndex;
-	var value=data.at(index);
-	var map=Alloy.createController('markedMap',{details:value});
 	map.getView().open();
 }
 
-$.header.rightNavButton.addEventListener('click', function(){
+$.header.rightNavButton.addEventListener('click', function() {
 	Alloy.Globals.menu.toggleRightDrawer();
-});
+}); 
