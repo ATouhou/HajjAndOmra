4109376253e6d4c263f14b76496f71aee9037dd6
muskteer <muskteer1983@gmail.com>
2013-09-30 16:10:40 +0200

    browser title done

--- app/controllers/browser.js
+++ app/controllers/browser.js
@@ -4,9 +4,20 @@ var image = args.image;
 var video = args.video;
 
 if (video) {
-	$.show.setUrl(video);
+	if (video.length < 18) {
+		$.header.title.text = video;
+		$.show.setUrl(video);
+	} else {
+		var x = video.substring(6, 21);
+		$.header.title.text = x;
+		$.show.setUrl(video);
+	}
 } else {
+	$.header.title.text = image;
 	$.show.setHtml('<html><body> <img src=image /> </body></html>');
-        
 }
+
+$.show.addEventListener('load', function(e) {
+	$.header.title.text = $.show.evalJS("document.title");
+});
 
