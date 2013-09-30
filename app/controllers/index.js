e861988c3aa7d95d421aefef9daed2c57ba2ebbd
islamfotouh <eslam.a.elftoh@gmail.com>
2013-09-30 12:39:06 +0200

    index.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,21 +1,28 @@
 var actionbar = require('actionbar');
 
 if (OS_IOS && Alloy.isHandheld) {
-	Alloy.Globals.navgroup = $.navgroup;	
+	Alloy.Globals.navgroup = $.navgroup;
 }
 
-
-if (OS_ANDROID){
+if (OS_ANDROID) {
 	$.main.open();
 } else {
 	$.index.open();
 }
 
-
-Alloy.Globals.menu = $.menu; 
+Alloy.Globals.menu = $.menu;
 
 $.menu.init({
-  rightDrawer:  Alloy.createController('home/rightmenu').getView(),
-  content: Alloy.createController('home/home').getView()
+	rightDrawer : Alloy.createController('home/rightmenu').getView(),
+	content : Alloy.createController('home/home').getView()
 });
+
+var catagorys = Alloy.createCollection('Catagory');
+catagorys.fetch();
+var catgCount = catagorys.length;
+
+var items = Alloy.createCollection('Item');
+items.fetch();
+var itmCount = items.length;
+
 
