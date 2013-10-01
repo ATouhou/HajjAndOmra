23b7d085c7802cfa6d3874cb45f8510ee3906c1c
muskteer <muskteer1983@gmail.com>
2013-10-01 14:07:41 +0200

    change the title of list to the catagory name

--- app/controllers/List.js
+++ app/controllers/List.js
@@ -1,16 +1,18 @@
-$.header.rightNavButton.addEventListener('click', function(){
+$.header.rightNavButton.addEventListener('click', function() {
 	Alloy.Globals.menu.toggleRightDrawer();
 });
 
 var args = arguments[0] || {};
-var catagoryID = args.type;
+var catagoryID = args.data.get('id');
 var collection = Alloy.Collections.item;
 collection.fetch({
-		// query: 'SELECT * FROM model WHERE columns
-		query: {
-			statement: 'SELECT * FROM Item WHERE catagoryID = ?',
-			params: [catagoryID]
-		}
-	});
+	// query: 'SELECT * FROM model WHERE columns
+	query : {
+		statement : 'SELECT * FROM Item WHERE catagoryID = ?',
+		params : [catagoryID]
+	}
+});
 
 Alloy.Collections.item.fetch();
+
+$.header.title.text=args.data.get('name');
